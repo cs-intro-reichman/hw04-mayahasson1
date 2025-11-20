169c4d9a9bdd7a8ede4ff276fe267ba66cacd4fe
Maya Hasson <maya.hasson@post.runi.ac.il>
2025-11-20 21:18:38 +0200

    assignment

--- MyString.java
+++ MyString.java
@@ -18,13 +18,47 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String newS="";
+        for(int i=0;i<str.length();i++)
+        {
+            if(str.charAt(i)>= 'A'&&str.charAt(i)<='Z')
+            {
+                newS+=(char)(str.charAt(i)+32);
+            }
+            else
+            {
+                newS+=str.charAt(i);
+            }
+        }
+        return newS;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        if (str1==null||str2==null)
+        {
+            return false;
+        }
+        if(str1.length()<str2.length())
+        {
+            return false;
+        }
+        for(int i=0;i<=str1.length()-str2.length();i++)
+        {
+                boolean same=true;
+                for(int j=0;j<str2.length();j++)
+                {
+                    if(str1.charAt(i+j)!=str2.charAt(j))
+                    {
+                        same=false;
+                        break;
+                    }
+                }
+                if(same)
+                {
+                    return true;
+                }
+            }
         return false;
     }
 }
--- Primes.java
+++ Primes.java
@@ -1,5 +1,58 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+    int num=Integer.parseInt(args[0]);
+    boolean[] newP=primeAr(num);
+    toPrint(newP);
     }
+    public static void toPrint(boolean[] newP)
+    {
+    int count=0;
+    System.out.println("Prime numbers up to "+(newP.length-1)+":");
+    for(int i=0;i<newP.length;i++)
+    {
+        if(newP[i])
+        {
+            System.out.println(i);
+            count++;
+
+        }
+    }
+    int prec=(int)(((double)count/newP.length)*100);
+    System.out.println("There are "+count+" primes between 2 and "+(newP.length-1)+" ("+prec+"% are primes)");
+    }
+    public static boolean[] newAr(int num)
+    {
+    boolean[] prime=new boolean[num];
+    prime[0]=false;
+    prime[1]=false;
+    for(int i=2;i<prime.length;i++)
+    {
+        prime[i]=true;
+    }
+    return prime;
+    }
+    public static boolean[] primeAr(int num)
+    {
+        boolean[] newP= newAr(num+1);
+        double sq=(int)Math.sqrt(newP.length);
+        for(int i=2;i<=sq;i++)
+        {
+            if(newP[i])
+            {
+                divNum(newP,i);
+
+            }
+        }
+        return newP;
+    }
+    public static void divNum(boolean[] newP, int num)
+    {
+        for(int i=num*num;i<newP.length;i+=num)
+        {
+            {
+                newP[i]=false;
+            }
+        }
+    }
+    
 }
